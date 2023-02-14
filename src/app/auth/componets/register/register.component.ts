0834e6d5357e2e72668dc0abe84bb172e0e71223
DESKTOP-33MVP3Q\Talamino <csi21-atallez@colegioaltair.net>
2023-02-14 18:13:28 +0100

    no message

--- src/app/auth/componets/register/register.component.ts
+++ src/app/auth/componets/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { AbstractControl, FormControl, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators, ValidatorFn } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '../../core/services/auth.service';
 
@@ -10,26 +10,58 @@ import { AuthService } from '../../core/services/auth.service';
 })
 export class RegisterComponent implements OnInit {
 
-  formReg: FormGroup;
+  //Propiedades
+  datosformReg: any;
 
-  constructor(private authService: AuthService, private router: Router) {
-    this.formReg = new FormGroup({
-      email: new FormControl(),
-      password: new FormControl()
-    })
+  //Formulario Reactivo
+  formReg = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', Validators.required],
+    passwordR: ['', Validators.required]
+  })
+
+
+  constructor(
+   private authService: AuthService, 
+   private router: Router,
+   private fb: FormBuilder) {
+    
   }
 
   ngOnInit(): void {
+    // Añadir validación personalizada al campo passwordR para comprobar que los campos password y passwordR son iguales
+    setTimeout(() => {
+      if (this.formReg.hasError('passwordsNotMatch')) {
+        console.log('Las contraseñas no coinciden');
+        return;
+      }
+    });
   }
 
-  
   onSubmit() {
-    this.authService.register(this.formReg.value)
+
+    this.datosformReg = this.formReg.value;
+
+    if (this.formReg.hasError('passwordsNotMatch')) {
+      console.log('Las contraseñas no coinciden');
+      return;
+    }
+
+    this.authService.register(this.datosformReg)
       .then(response => {
         console.log(response);
         this.router.navigate(['/']);
       })
       .catch(error => console.log(error));
+  }
+
+  // Función para validar que los campos password y passwordR son iguales
+  private checkPasswordsMatch(passwordControl: AbstractControl): ValidatorFn {
+    return (control: AbstractControl): {[key: string]: any} | null => {
+      const password = passwordControl.value;
+      const passwordR = control.value;
+      return password === passwordR ? null : {'passwordsNotMatch': true};
+    };
   }
 
 }
